2512076ca2639a98a191c067cd64aeec84afb29d
Jun Yuan <yuanjuns@outlook.com>
2021-08-22 17:41:14 +0800

    use azure b2c

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+import 'profile.dart';
+
 final FlutterAppAuth appAuth = FlutterAppAuth();
 final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
 
@@ -12,8 +14,9 @@ final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
 ///           Auth0 Variables
 /// -----------------------------------
 
-const AUTH0_DOMAIN = 'flutter.jp.auth0.com';
-const AUTH0_CLIENT_ID = 'EkPZ4XMB1NVnrqFpPm96wiWjjyqxXf5t';
+const AUTH0_DOMAIN =
+    'login.microsoftonline.com/9188040d-6c67-4c5b-b112-36a304b66dad/v2.0';
+const AUTH0_CLIENT_ID = 'a9a03c38-e002-4426-b817-90d1cc212f3b';
 
 const AUTH0_REDIRECT_URI = 'com.auth0.flutterdemo://login-callback';
 const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
@@ -43,6 +46,37 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: Text('Auth0 Demo'),
+          actions: isBusy
+              ? <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.sip_outlined),
+                    tooltip: 'Show Snackbar',
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('This is a snackbar')));
+                    },
+                  )
+                ]
+              : isLoggedIn
+                  ? <Widget>[
+                      IconButton(
+                        icon: const Icon(Icons.perm_identity),
+                        tooltip: 'My Profile',
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text('This is a snackbar')));
+                        },
+                      )
+                    ]
+                  : <Widget>[
+                      ElevatedButton(
+                        child: const Text("Login"),
+                        onPressed: () {
+                          loginAction();
+                        },
+                      )
+                    ],
         ),
         body: Center(
           child: isBusy
@@ -64,7 +98,8 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future<Map<String, dynamic>> getUserDetails(String accessToken) async {
-    final url = 'https://$AUTH0_DOMAIN/userinfo';
+    final url =
+        'https://graph.microsoft.com/oidc/userinfo'; //'https://$AUTH0_DOMAIN/userinfo';
     final response = await http.get(
       Uri.parse(url),
       headers: {'Authorization': 'Bearer $accessToken'},
@@ -161,48 +196,6 @@ class _MyAppState extends State<MyApp> {
       print('error on refresh token: $e - stack: $s');
       logoutAction();
     }
-  }
-}
-
-/// -----------------------------------
-///           Profile Widget
-/// -----------------------------------
-
-class Profile extends StatelessWidget {
-  final logoutAction;
-  final String name;
-  final String picture;
-
-  Profile(this.logoutAction, this.name, this.picture);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          width: 150,
-          height: 150,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.blue, width: 4.0),
-            shape: BoxShape.circle,
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(picture),
-            ),
-          ),
-        ),
-        SizedBox(height: 24.0),
-        Text('Name: $name'),
-        SizedBox(height: 48.0),
-        RaisedButton(
-          onPressed: () {
-            logoutAction();
-          },
-          child: Text('Logout'),
-        ),
-      ],
-    );
   }
 }
 
